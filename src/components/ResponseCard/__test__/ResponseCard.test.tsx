130b7a983e91380098b44ab46b4894e4b911f807
nijason7789 <nijason7789@gmail.com>
2024-08-08 23:55:29 +0800

    fix typo

--- src/components/ResponseCard/__test__/ResponseCard.test.tsx
+++ src/components/ResponseCard/__test__/ResponseCard.test.tsx
@@ -4,15 +4,15 @@ import ResponseCard from '../ResponseCard';
 
 const mockedComment = {
     original: "This is mocked comment",
-    tranlsation: "This is mocked comment's translation"
+    translation: "This is mocked comment's translation"
   }
 
 describe ('Response card unit test', () =>{
     it('Response Card were rendered correctly', () => {
         const { getByText } = render(
-            <ResponseCard title={mockedComment.original as string} text={mockedComment.tranlsation as string} />
+            <ResponseCard title={mockedComment.original} text={mockedComment.translation} />
         );
     expect(getByText(mockedComment.original)).toBeVisible();
-    expect(getByText(mockedComment.tranlsation)).toBeVisible();
+    expect(getByText(mockedComment.translation)).toBeVisible();
     });
 })
